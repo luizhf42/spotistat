994f6a6b2d75c2758b9ade1a1952a9b0670df211
Luiz Henrique <luizhf42@gmail.com>
2022-10-14 15:30:46 -0300

    feat: add the method to request authorization

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 @click="requestAuthorization">bora</h1>
   </div>
 </template>
+const accountURL = "https://accounts.spotify.com";
+const clientId = config.public.clientId;
+const redirectURI = "http://localhost:3000/";
+const scope =
+  "user-read-playback-state user-follow-read user-read-currently-playing user-read-playback-position user-top-read user-read-recently-played";
+const requestAuthorization = () => {
+  window.location.href = `${accountURL}/authorize?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectURI}`;
+};
